ac510e8dc843c7076c04960dd71324a03a0ccf44
Yuzobra <yuri.zoel@hotmail.com>
2019-10-08 19:49:03 -0400

    compilable version

--- processManager.c
+++ processManager.c
@@ -18,10 +18,10 @@
 
 ProcInfo * getProcInfo(int fd[]);
 
-void AlrmHandler(int sinal)
-{
-	flag = 1;
-}
+
+
+
+void AlrmHandler(int sinal);
 
 int flag = 0; //flag que diz se o processo deve escalonar ou olhar a pipe
 
@@ -50,46 +50,64 @@ int main (int argc, char *argv[]) {
 
 			printf("Iniciando processo 1\n");
 		 	char arg[12];
-		 	char path[] = "/root/Codes/SO/Trab1/reader"; 
 			char * arr[3];
 		 	sprintf(arg,"%d",fd[1]);
-			arr[0] = path;
+			arr[0] = "./reader";
 			arr[1] = arg;
 			arr[2] = NULL;
 			//close(fd[0]);
-			execv("./reader", arr);
+			execv(arr[0], arr);
 		}
 		
 		else if(i == 1) /* processo 2 */{
-			No * listaProcs = criaLista();
+			No * listaProcs = CriaLista();
+			int pidProc;
 			ProcInfo *procInfo;
-			signal(SIGALMR, alarmhandler); 
+			signal(SIGALRM, AlrmHandler); 
 			while(1){
-				if(flag==0){
+				if(flag==0) /* Pode se inserir um novo processo na lista de processos*/ {
 					
 					procInfo = getProcInfo(fd);									//precisamos fazer isso ser non-blocking (trocar pra FIFO talvez?)
 					printf("Nome do Processo: %s\n", procInfo->nomeProc);
 					printf("Tipo do Processo: %s\n", procInfo->tipoProc);
 					
-					char nome[] = "./";
-					strcat(nome,procInfo->NomeProc);
-					if ((pid = fork()) == 0) {
-						char *const * arg = {NULL}
-						execve(nome;arg;NULL);
+					char *nome = (char*) malloc (sizeof(char)*(3+strlen(procInfo->nomeProc))); 
+					nome[0] = '.';
+					nome[1] = '/';
+					nome[2] = '\0';
+					
+					strcat(nome,procInfo->nomeProc);
+					if ((pidProc = fork()) == 0) /* Cria o novo processo */ {
+						char *arg[] = {nome,NULL};
+						execv(arg[0], arg);
 					}
-					kill(pid, SIGSTOP); //interrompe o recém nascido
-					if(procInfo->I==NULL){
-						int prioridade = procInfo->PR;
-						if(prioridade == NULL) {prioridade = 8;}
-						lista = insereElemento(listaProcs, pid, prioridade);
+					else{
+						kill(pidProc, SIGSTOP); //interrompe o recém nascido
+						if(procInfo->I==NULL) /* Prioridade ou Round Robin */ {
+							int prioridade;
+							if(procInfo->PR == NULL) /* Round Robin */ {prioridade = 8;}
+							else /* Prioridade */{prioridade = atoi(procInfo->PR);}
+							listaProcs = insereElemento(listaProcs, pid, prioridade);  //este insereElemento vai inserir na posição correta (?)
+						}
+						else /* Real Time*/ {
+						
+						
+						}
 					}
 					
 				}
-				else{
+				else /* Um processo acabou de ser interrompido (é para isso que serve esse bloco né ?? || honestamente não me lembro mais ) */ {
+					
+					/*
+					OBS:ISTO DEVE SER ENVOLTO EM UM MUTEX, PARA QUE NAO SEJA INTERROMPIDO CASO HAJA UM NOVO SIGALRM
+					O SIGALRM TMB DEVE SER ENVOLTO NESTE MUTEX, PARA QUE AGUADE O FIM DA EXECUÇÃO DESTE BLOCO
 					
 					
 					
-					flag == 0;
+					
+					*/
+					
+					flag = 0;
 				}
 			}	
 		}
@@ -108,14 +126,12 @@ int main (int argc, char *argv[]) {
 
 
 
-
-
 ProcInfo * getProcInfo(int fd[]){
 	int sizeOfProcInfo;
 	char serializedProcInfo[100];
 	ProcInfo *procInfo = (ProcInfo*)malloc(sizeof(ProcInfo));
 
-	
+	printf("Estou lendo do pipe\n");
 	read(fd[0],&sizeOfProcInfo,sizeof(int));
 	read(fd[0],serializedProcInfo,sizeOfProcInfo);
 	
@@ -125,5 +141,11 @@ ProcInfo * getProcInfo(int fd[]){
 
 	procInfo = deserializeProcInfo(serializedProcInfo);
 	return procInfo;
+}
+
+
+void AlrmHandler(int sinal)
+{
+	flag = 1;
 }
 
