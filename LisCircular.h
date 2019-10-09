a6b5bc62b363aecbbe350a7e28144fb522cd4cc7
GraekTarmikos <49797882+GraekTarmikos@users.noreply.github.com>
2019-10-08 21:32:24 -0300

    Comentários da lista circular

--- LisCircular.h
+++ LisCircular.h
@@ -6,17 +6,24 @@ typedef struct no {
 	struct no *ant;		//Ponteiro pro nó anterior
 }No;
 
+/*Função que cria a lista com um único elemento nulo*/
 No* CriaLista();
 
+/*Função que insere um elemento*/
 No* insereElemento(No *lista, int pid, int prio);
 
+/*Função que remove o elemento atual da lista*/
 No* removeElemento(No *lista);
 
+/*Função que avança o nó corrente*/
 No* proxElem(No *lista);
 
+/*Função que recua o nó corrente*/
 No* antElem(No *lista);
 
+/*Função que libera a lista inteira*/
 void destroiLista(No *lista);
 
+/*Função que imprime a lista inteira para propósitos de teste (podemos remover depois)*/
 void printaLista(No *lista);
 
